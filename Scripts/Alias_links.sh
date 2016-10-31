4eaa4c323fecf2e9eb47d6275e3d787db99a7b6f
Ibexguy <mathieu.robin@uzh.ch>
2016-10-31 18:01:52 +0100

    Update Alias_links.sh

--- Scripts/Alias_links.sh
+++ Scripts/Alias_links.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
 
-raw_scr=/Home_Ibex/Rawdata/Screening
-data_w=/Home_Ibex/Work/multiplex
+#make Project folder
+Project=NameofProject
+
+while $Project 
+do 
+mkdir /$Project/Rawdata
+mkdir /$Project/
+raw_scr=/$Project/Rawdata/Screening
+data_w=/$Project/Work/multiplex
 work=/Home_Ibex/Work
 trim=/Home_Ibex/Software/Trimmomatic-0.36/trimmomatic-0.36.jar
 softw=/Home_Ibex/Software
